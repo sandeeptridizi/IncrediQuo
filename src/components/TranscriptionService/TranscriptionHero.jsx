c8673b6cd72f53d9673f134e31920e881ff2f1b9
soujanyaramdeni <soujanyaramdeni.com>
2025-11-28 16:19:13 +0530

    login get credentials

--- src/components/TranscriptionService/TranscriptionHero.jsx
+++ src/components/TranscriptionService/TranscriptionHero.jsx
@@ -6,7 +6,7 @@ import "../../appStyles/Services/TranscriptionHero.css";
 import visionCard from "../../assets/icons/Group 5.png";
 import goalCard from "../../assets/icons/Group 9.png";
 import BulbIcon from "../../assets/services/ServiceIcon.png";
-import BulbIcon2 from "../../assets/services/ServiceIcon1.png";
+import BulbIcon2 from "../../assets/services/serviceIcon1.png";
 
 const TranscriptionHero = ({ onOpenContact }) => {
   const handleGetStarted = () => {
